7f2d4178cc03aef402d6bcbc45c0d235da30ddc3
Hasan Akniyev <lbrain.dev@gmail.com>
2011-09-11 14:37:47 +0400

    References added

---
+++ gymn-journal/Domain/Abstract/IStudentRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Domain.Entities;
+
+namespace Domain {
+    interface IStudentRepository {
+        IQueryable<Student> Students { get; }
+    }
+}
---
+++ gymn-journal/Domain/Entities/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Domain.Entities {
+    class Student {
+        public int StudentID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+    }
+}
